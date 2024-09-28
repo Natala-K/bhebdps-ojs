6a69c870280a6b7c802b09b051140dd9e1c5bb62
Kolmy <test@neto.com>
2024-09-28 23:35:09 +0300

    added functions vysshego porjadku

--- JS-Koans/topics/4 Функции высшего порядка/about_higher_order_functions.js
+++ JS-Koans/topics/4 Функции высшего порядка/about_higher_order_functions.js
@@ -4,27 +4,31 @@ describe("About Higher Order Functions (about_higher_order_functions.js)", funct
     let numbers = [1,2,3];
     let odd = numbers.filter(function (x) { return x % 2 !== 0 });
 
-    expect(odd).toEqual(FILL_ME_IN);
-    expect(odd.length).toBe(FILL_ME_IN);
-    expect(numbers.length).toBe(FILL_ME_IN);
+    expect(odd).toEqual([1, 3]);
+    expect(odd.length).toBe(2);
+    expect(numbers.length).toBe(3);
   });
 
   it("should use 'map' to transform each element", function () {
     let numbers = [1, 2, 3];
     let numbersPlus1 = numbers.map(function(x) { return x + 1 });
 
-    expect(numbersPlus1).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(numbersPlus1).toEqual([2, 3, 4]);
+    expect(numbers).toEqual([1, 2, 3]);
   });
+
+
 
   it("should use 'reduce' to update the same result on each iteration", function () {
     let numbers = [1, 2, 3];
     let reduction = numbers.reduce(
             function(/* result from last call */ memo, /* current */ x) { return memo + x }, /* initial */ 0);
 
-    expect(reduction).toBe(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(reduction).toBe(6);
+    expect(numbers).toEqual([1, 2, 3]);
   });
+
+
 
   it("should use 'forEach' for simple iteration", function () {
     let numbers = [1,2,3];
@@ -35,9 +39,11 @@ describe("About Higher Order Functions (about_higher_order_functions.js)", funct
 
     numbers.forEach(isEven);
 
-    expect(msg).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(msg).toEqual("falsetruefalse");
+    expect(numbers).toEqual([1, 2, 3]);
   });
+
+
 
   it("should use 'every' to test whether all items pass condition", function () {
     let onlyEven = [2,4,6];
@@ -45,9 +51,12 @@ describe("About Higher Order Functions (about_higher_order_functions.js)", funct
 
     let isEven = function(x) { return x % 2 === 0 };
 
-    expect(onlyEven.every(isEven)).toBe(FILL_ME_IN);
-    expect(mixedBag.every(isEven)).toBe(FILL_ME_IN);
+    expect(onlyEven.every(isEven)).toBe(true);
+    expect(mixedBag.every(isEven)).toBe(false);
   });
+
+
+
 
   it("should use 'some' to test if any items passes condition" , function () {
     let onlyEven = [2,4,6];
@@ -55,20 +64,23 @@ describe("About Higher Order Functions (about_higher_order_functions.js)", funct
 
     let isEven = function(x) { return x % 2 === 0 };
 
-    expect(onlyEven.some(isEven)).toBe(FILL_ME_IN);
-    expect(mixedBag.some(isEven)).toBe(FILL_ME_IN);
+    expect(onlyEven.some(isEven)).toBe(true);
+    expect(mixedBag.some(isEven)).toBe(true);
   });
 
+
+
   it("should use flatten to make nested arrays easy to work with", function() {
-      expect([ [1, 2], [3, 4] ].flat()).toEqual(FILL_ME_IN);
+      expect([ [1, 2], [3, 4] ].flat()).toEqual([1, 2, 3, 4]);
   });
+
 
   it("should use flat() ... reduce() to use multiple higher order functions", function() {
       let result = [ [0, 1], 2 ].flat()
         .map(function(x) { return x+1 } )
         .reduce(function (sum, x) { return sum + x });
 
-      expect(result).toEqual(FILL_ME_IN);
+      expect(result).toEqual(6);
   });
 
 });
